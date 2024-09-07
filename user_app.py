9ebd347f4bec0692348c8d8def2cfb0e645ee8b4
SHAHIN SHABAB P <141617747+shahinshabab@users.noreply.github.com>
2024-09-07 13:56:40 +0530

    Update user_app.py
    streamlit bridge

--- user_app.py
+++ user_app.py
@@ -1,46 +1,12 @@
 import streamlit as st
+from st_bridge import bridge, html
 
-# Define the URL of your local server
-server_url = 'http://192.168.1.14:5000/'
+data = bridge("my-bridge", default="no button is clicked")
 
-# Streamlit app layout
-st.title('Send HTTP Request with JavaScript')
+html("""
+<button onClick="stBridges.send('my-bridge', 'button 1 is clicked')">Button 1</button>
+<button onClick="stBridges.send('my-bridge', 'button 2 is clicked')">Button 2</button>
+<button onClick="stBridges.send('my-bridge', 'button 3 is clicked')">Button 3</button>
+""")
 
-# Simplified HTML and JavaScript embedded in the Streamlit app
-html_code = f"""
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Send Request</title>
-</head>
-<body>
-    <h2>Send HTTP Request</h2>
-    <button onclick="sendRequest()">Send Request</button>
-    <p id="response"></p>
-    
-    <script>
-        const serverUrl = '{server_url}';
-        
-        function sendRequest() {{
-            fetch(serverUrl, {{
-                method: 'POST',
-                headers: {{
-                    'Content-Type': 'application/json'
-                }},
-                body: JSON.stringify({{key1: 'value1', key2: 'value2'}})
-            }})
-            .then(response => response.json())
-            .then(data => {{
-                document.getElementById('response').innerText = 'Response from server: ' + JSON.stringify(data);
-            }})
-            .catch(error => {{
-                document.getElementById('response').innerText = 'Error: ' + error;
-            }});
-        }}
-    </script>
-</body>
-</html>
-"""
-
-# Render HTML with JavaScript in the Streamlit app
-st.markdown(html_code, unsafe_allow_html=True)
+st.write(data)
