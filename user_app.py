b501a652846ada714cda30354de15e9d96252e27
SHAHIN SHABAB P <141617747+shahinshabab@users.noreply.github.com>
2024-09-07 13:22:26 +0530

    Update user_app.py
    sending http pop up

--- user_app.py
+++ user_app.py
@@ -1,16 +1,26 @@
 import streamlit as st
 import requests
+import json
 
-def send_request():
+st.title('Send JSON Data')
+
+# Input fields for JSON data
+key1 = st.text_input('Key 1')
+value1 = st.text_input('Value 1')
+key2 = st.text_input('Key 2')
+value2 = st.text_input('Value 2')
+
+if st.button('Send JSON Request'):
+    # Create JSON data
+    data = {
+        key1: value1,
+        key2: value2
+    }
+    # Replace <your-local-ip>:5000 with your server URL
+    url = 'http://192.168.1.14:5000/'
+    
     try:
-        # Replace <your-local-ip> with the IP address of your local server
-        response = requests.get('http://192.168.1.14:5000/')
-        return response.text
+        response = requests.post(url, json=data)
+        st.write('Response from server:', response.text)
     except requests.RequestException as e:
-        return f'Error: {e}'
-
-st.title('Streamlit HTTP Request Example')
-
-if st.button('Send Request'):
-    result = send_request()
-    st.write(result)
+        st.error(f'Error: {e}')
